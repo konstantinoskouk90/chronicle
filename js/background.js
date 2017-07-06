243152ff859eb6c9049e4d118de96013221e4632
konstantinoskouk90 <konstantinoskouk90@gmail.com>
2017-07-06 22:01:51 +0100

    first commit

---
+++ js/background.js
@@ -0,0 +1,179 @@
+/*jshint esnext: true */
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if ((message.from === "content")) {
+        if (!!message.found) {
+            delete localStorage.playlist_scanned;
+            localStorage.setItem("playlist_scanned", message.data);
+        } else {
+            delete localStorage.playlist_scanned;
+        }
+        chrome.runtime.sendMessage({ data: "VIDEOS_SCANNED" });
+    }
+});
+
+var chromeExtension = {
+
+    activeTab: function() {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            if (!/^chrome\:\/\//.test(tabs[0].url)) {
+                chrome.tabs.executeScript(tabs[0].id, {
+                    file: "js/content.js"
+                });
+            } else {
+                chrome.runtime.sendMessage({ data: "CANNOT_SCAN_PAGE" });
+            }
+        });
+    },
+    constrObj: function(title, date_modified, description, status, lastAdded, count_plays, num_vids, thumbnails, img_src, link, action) {
+
+        var msg, objConstr = {},
+            objStored = JSON.parse(localStorage.playlist),
+            objKeys = Object.keys(objStored);
+
+        if (!objKeys.length) {
+            objConstr[title] = {};
+            objConstr[title].date_modified = date_modified;
+            objConstr[title].description = description;
+            objConstr[title].status = status;
+            objConstr[title].lastAdded = lastAdded;
+            objConstr[title].plays = count_plays;
+            objConstr[title].videos = num_vids;
+            objConstr[title].thumbnails = thumbnails;
+            objConstr[title].image = img_src;
+            objConstr[title].link = link;
+            localStorage.setItem("playlist", JSON.stringify(objConstr));
+        } else {
+            objStored[title] = {};
+            objStored[title].date_modified = date_modified;
+            objStored[title].description = description;
+            objStored[title].status = status;
+            objStored[title].lastAdded = lastAdded;
+            objStored[title].plays = count_plays;
+            objStored[title].videos = num_vids;
+            objStored[title].thumbnails = thumbnails;
+            objStored[title].image = img_src;
+            objStored[title].link = link;
+            localStorage.setItem("playlist", JSON.stringify(objStored));
+        }
+
+        switch (action) {
+            case "ADD_VIDEO":
+                msg = "ADDED_VIDEO";
+                break;
+            case "PLAY_PLAYLIST":
+                msg = "PLAYED_PLAYLIST";
+                break;
+            case "CREATE_PLAYLIST":
+                msg = "CREATED_PLAYLIST";
+                break;
+            case "SAVE_PLAYLIST":
+                msg = "SAVED_PLAYLIST";
+                break;
+            case "SAVE_VIDEO_THUMB":
+                msg = "SAVED_VIDEO_THUMB";
+                break;
+            case "SAVE_VIDEO_POS_THUMB":
+                msg = "SAVED_VIDEO_POS_THUMB";
+                break;
+            case "DELETE_VIDEO":
+                msg = "DELETED_VIDEO";
+                break;
+            case "SCAN_ADD_VIDEO":
+                msg = "SCANNED_ADDED_VIDEO";
+                break;
+        }
+
+        if (action !== undefined) {
+            chrome.runtime.sendMessage({ data: msg });
+        }
+    },
+    updatePlaylist: function(caller) {
+
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+
+            var all_IDs = JSON.parse(localStorage.playlist)[caller.playlist_name].link || "";
+            var numVids = JSON.parse(localStorage.playlist)[caller.playlist_name].videos;
+
+            if (/\/\/www\.youtube\.com\/watch.*v\=/.test(tabs[0].url)) {
+
+                var video_id = tabs[0].url.match(/watch.*v\=([^&]+)/)[1];
+
+                if (!all_IDs.length) {
+                    caller.playlist = video_id;
+                    chromeExtension.createPlaylist(caller);
+                } else {
+                    var prod = new RegExp(video_id);
+                    if (!prod.test(all_IDs) && numVids < 50) {
+                        caller.playlist = all_IDs.match(/video_ids=(.*)/)[1] + "," + video_id;
+                        chromeExtension.createPlaylist(caller);
+                    } else if (numVids === 50) {
+                        chrome.runtime.sendMessage({ data: "MAX_VIDEO" });
+                    } else if (prod.test(all_IDs)) {
+                        chrome.runtime.sendMessage({ data: "VIDEO_EXISTS" });
+                    }
+                }
+            } else {
+                chrome.runtime.sendMessage({ data: "INVALID_PAGE" });
+            }
+        });
+    },
+    createPlaylist: function(caller) {
+
+        var title, description, img_src, link, num_vids, thumbnails, count_plays, extract_ids;
+
+        if (caller.playlist !== undefined) {
+            extract_ids = caller.playlist;
+        } else {
+            extract_ids = "";
+        }
+
+        if (caller.playlist_plays !== undefined) {
+            count_plays = caller.playlist_plays;
+        } else {
+            count_plays = "0";
+        }
+
+        if (caller.thumbnails !== undefined) {
+            thumbnails = caller.thumbnails;
+        } else {
+            thumbnails = "";
+        }
+
+        title = caller.playlist_name;
+        description = caller.playlist_description;
+        img_src = caller.playlist_image;
+        var date_modified = new Date();
+
+        if (!!extract_ids.length) {
+            link = "https://www.youtube.com/watch_videos?&title=" + title + "&video_ids=" + extract_ids;
+            var lastAddedLink = "https://www.youtube.com/embed/" + extract_ids.split(",")[(extract_ids.split(",").length) - 1];
+            status = "enabled";
+            num_vids = extract_ids.split(",").length;
+            xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    if (JSON.parse(xhr.responseText).title !== undefined) {
+                        chromeExtension.constrObj(title, date_modified, description, status, encodeURIComponent(JSON.parse(xhr.responseText).title), count_plays, num_vids, thumbnails, img_src, link, caller.playlist_action);
+                    } else {
+                        if (caller.playlist_action === "ADD_VIDEO") {
+                            chrome.runtime.sendMessage({ data: "VIDEO_UNAVAILABLE" });
+                        }
+                        if (caller.playlist_action === "PLAY_PLAYLIST") {
+                            chromeExtension.constrObj(title, date_modified, description, status, encodeURIComponent(JSON.parse(xhr.responseText).title), count_plays, num_vids, thumbnails, img_src, link, caller.playlist_action);
+                        }
+                    }
+                }
+            };
+            xhr.open("GET", "https://noembed.com/embed?format=json&url=" + encodeURIComponent(lastAddedLink));
+            xhr.send();
+        } else {
+            link = "";
+            status = "disabled";
+            num_vids = 0;
+            thumbnails = "";
+            var lastAdded = "";
+            this.constrObj(title, date_modified, description, status, lastAdded, count_plays, num_vids, thumbnails, img_src, link, caller.playlist_action);
+        }
+    },
+};
---
+++ js/content.js
@@ -0,0 +1,88 @@
+function scrapeDOM() {
+
+    var id, ifrm_id, link, src, ytIDs = [],
+        fb_links = document.querySelectorAll(".mbs._6m6._2cnj._5s6c a").length,
+        o_links = document.querySelectorAll("a").length,
+        b_links = document.querySelectorAll(".vrhdata").length,
+        iframes = document.querySelectorAll("iframe").length,
+        links;
+
+    if (fb_links > 0) {
+        links = fb_links;
+    } else if (o_links > 0 && location.hostname !== "www.bing.com") {
+        links = o_links;
+    } else if (b_links > 0 && location.hostname === "www.bing.com") {
+        links = b_links;
+    }
+
+    if (links > 0) {
+
+        for (var i = 0; i < links; i++) {
+
+            if (location.hostname === "www.facebook.com" && !!document.querySelectorAll(".mbs._6m6._2cnj._5s6c a").length) {
+                link = document.querySelectorAll(".mbs._6m6._2cnj._5s6c a")[i].getAttribute("onmouseover");
+            } else if (!!document.querySelectorAll("a").length && location.hostname !== "www.bing.com" && !/yahoo\.com/.test(window.location.href)) {
+                link = document.querySelectorAll("a")[i].href;
+            } else if (/yahoo\.com/.test(window.location.href)) {
+                link = document.querySelectorAll("a")[i].getAttribute("data-rurl");
+            } else if (location.hostname === "www.bing.com") {
+                link = JSON.parse(document.querySelectorAll(".vrhdata")[i].getAttribute("vrhm")).purl;
+            }
+
+            if (!!link) {
+
+                if (location.hostname === "www.facebook.com" && (/\/watch\?v\=/.test(link)) && (!/attribution_link/.test(link))) {
+                    id = (link.match(/\/watch\?v\=([^&]+)/)[1]).split('");')[0].replace(/\#$/, "");
+                } else if (location.hostname === "www.facebook.com" && (/youtu\.be/.test(link)) && (!/feature/.test(link))) {
+                    id = (link.match(/youtu([^&]+)/)[1].match(/.*/)[0].split("/")[1].split('");')[0].replace(/\#$/, ""));
+                } else if (location.hostname === "www.facebook.com" && (/feature.*youtu\.be&v=/.test(link))) {
+                    id = (link.match(/&v=([^&]+)/)[1].split('");')[0].replace(/\#$/, ""));
+                } else if (location.hostname === "www.youtube.com" && /results\?search\_query\=/.test(location.href) && /\/watch\?v\=/.test(link)) {
+                    if ((!/feature=player-title/.test(document.querySelectorAll("a")[i].getAttribute("data-sessionlink"))) && !/ytp-next-button/.test(document.querySelectorAll("a")[i].getAttribute("class"))) {
+                        id = (link.match(/\/watch\?v\=([^&]+)/)[1]).replace(/\#$/, "");
+                    }
+                } else if ((location.hostname !== "www.facebook.com") && (location.hostname !== "www.bing.com") && (/\/watch\?v\=/.test(link))) {
+                    id = (link.match(/\/watch\?v\=([^&]+)/)[1]).replace(/\#$/, "");
+                } else if (location.hostname === "www.bing.com") {
+                    id = link.split("watch?v=")[1];
+                }
+
+                if (id !== undefined) {
+                    if (ytIDs.indexOf(id) < 0) {
+                        ytIDs.push(id);
+                    }
+                }
+            }
+        }
+    }
+
+    if (iframes > 0) {
+
+        for (var x = 0; x < iframes; x++) {
+
+            src = document.querySelectorAll("iframe")[x].src;
+
+            if (/www\.youtube\.com\/embed/.test(src)) {
+                ifrm_id = src.match(/embed\/([^&?]+)/)[1];
+            }
+            if (ifrm_id !== undefined) {
+                if (ytIDs.indexOf(ifrm_id) < 0) {
+                    ytIDs.push(ifrm_id);
+                }
+            }
+        }
+    }
+
+    //Callback
+    sendMessage(ytIDs);
+}
+
+function sendMessage(vids) {
+    if (!!vids.length) {
+        chrome.runtime.sendMessage({ from: "content", found: true, data: vids });
+    } else {
+        chrome.runtime.sendMessage({ from: "content", found: false });
+    }
+}
+
+scrapeDOM();
